542a9c7dc9fe0ad1f0a75a91821b171986fbb162
Ryan Brower <ryan.brower28@gmail.com>
2021-11-04 18:04:16 -0700

    control_actors_action

---
+++ batter/game/draw_actors_action.py
@@ -0,0 +1,30 @@
+<<<<<<< Updated upstream
+=======
+from game.action import Action
+
+class DrawActorsAction:
+    """A code template for drawing actors. The responsibility of this
+    class of objects is draw the actors on the screen.
+    
+    Stereotype:
+        Service Provider
+
+    Attributes:
+        _output_service (OutputService): An instance of OutputService.
+    """
+
+    def __init__(self, output_service):
+        """The class constructor.
+        
+        Args:
+            output_service (OutputService): An instance of OutputService.
+        """
+        self._output_service = output_service
+    
+    def execute(self, cast):
+        """uses the output_service attribute to draw the actors on the screen.
+
+        Args:
+            cast (dict): The game actors {key: tag, value: list}.
+        """
+>>>>>>> Stashed changes
